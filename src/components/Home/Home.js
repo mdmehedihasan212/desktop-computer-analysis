8f7bc60ce41030a609f2a5fc0ca357e8d821d7a3
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-04 01:58:57 +0600

    create two json file chart_data.json and review_data.json

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,17 +5,27 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <section className="product-container">
-            <div className="product-img">
-                <img src={image} alt="" />
+        <section>
+            <div className="product-container">
+                <div className="product-img">
+                    <img src={image} alt="" />
+                </div>
+                <div className="product-details">
+                    <h1 className='text-6xl font-bold'>The best desktop <br />
+                        <span className='text-blue-600'>under $500</span>
+                    </h1>
+                    <p>When buying a desktop computer, you'll need to consider all the usual things that you would consider for any other computer purchase. A solid processor (CPU) will keep everything you can throw at it running smoothly without stutters or crashes. Memory, also known as RAM, also plays a big role, especially if you plan on running several apps at the same time and gaming, where a computer needs to store and quickly access files associated with those apps. Last but not least is storage and if you want to keep lots of games or files on your computer, you'll need plenty of it.
+                    </p>
+                    <Link className='px-8 py-3 rounded bg-blue-600 text-white' to={'/'}>Details</Link>
+                </div>
             </div>
-            <div className="product-details">
-                <h1 className='text-6xl font-bold'>The best desktop <br />
-                    <span className='text-blue-600'>under $500</span>
-                </h1>
-                <p>When buying a desktop computer, you'll need to consider all the usual things that you would consider for any other computer purchase. A solid processor (CPU) will keep everything you can throw at it running smoothly without stutters or crashes. Memory, also known as RAM, also plays a big role, especially if you plan on running several apps at the same time and gaming, where a computer needs to store and quickly access files associated with those apps. Last but not least is storage and if you want to keep lots of games or files on your computer, you'll need plenty of it.
-                </p>
-                <Link className='px-8 py-3 rounded bg-blue-600 text-white' to={'/'}>Details</Link>
+            <div className="review-container">
+                <div className="review-cart">
+                    <h1>Review Cart</h1>
+                </div>
+                <div className="review-btn">
+                    <h1>See all review</h1>
+                </div>
             </div>
         </section>
     );
