f00b4240755f0cc105d2741f6f8cf3b6380e0574
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-04 15:55:23 +0600

    add dashboard component and create four diffrent cart

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,10 +1,113 @@
 import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const data = [
+    {
+        "month": "Mar",
+        "investment": 100000,
+        "sell": 241,
+        "revenue": 10401
+    },
+    {
+        "month": "Apr",
+        "investment": 200000,
+        "sell": 423,
+        "revenue": 24500
+    },
+    {
+        "month": "May",
+        "investment": 500000,
+        "sell": 726,
+        "revenue": 67010
+    },
+    {
+        "month": "Jun",
+        "investment": 500000,
+        "sell": 529,
+        "revenue": 40405
+    },
+    {
+        "month": "Jul",
+        "investment": 600000,
+        "sell": 601,
+        "revenue": 50900
+    },
+    {
+        "month": "Aug",
+        "investment": 700000,
+        "sell": 670,
+        "revenue": 61000
+    }
+]
 
 const DashBoard = () => {
     return (
-        <div>
-
-        </div>
+        <section className='grid grid-cols-2 gap-12 mt-12 px-12'>
+            <div>
+                <h1 className='text-2xl font-normal mb-3 mx-16'>Investment vs Revenue (Area Chart)</h1>
+                <AreaChart width={625} height={250} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend />
+                    <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorInvestment)" />
+                    <Area type="monotone" dataKey="sell" stroke="#82ca9d" fillOpacity={1} fill="url(#colorSell)" />
+                    <Area type="monotone" dataKey="revenue" stroke="#ff7300" fillOpacity={1} fill="url(#colorRevenue)" />
+                </AreaChart>
+            </div>
+            <div>
+                <h1 className='text-2xl font-normal mb-3 mx-16'>Investment vs Revenue (Bar Chart)</h1>
+                <BarChart width={600} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="sell" fill="#82ca9d" />
+                    <Bar dataKey="revenue" fill="#ff7300" />
+                </BarChart>
+            </div>
+            <div>
+                <h1 className='text-2xl font-normal mb-3 mx-16'>Investment vs Revenue (Composed Chart)</h1>
+                <ComposedChart width={600} height={250} data={data}>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Area type="monotone" dataKey="investment" fill="#8884d8" stroke="#8884d8" />
+                    <Bar dataKey="sell" barSize={20} fill="#82ca9d" />
+                    <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+                </ComposedChart>
+            </div>
+            <div>
+                <h1 className='text-2xl font-normal mb-3 mx-16'>Investment vs Revenue (Line Chart)</h1>
+                <LineChart width={630} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+                </LineChart>
+            </div>
+        </section>
     );
 };
 
